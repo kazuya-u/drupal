65e07846dccd3e21c0a021687d9e25992985489f
mochiya-umeki <umeki@mochiya.co>
2023-01-18 02:38:39 +0900

    Add sender information.

--- poc.module/docroot/modules/custom/jmeter/src/Form/JmeterForm.php
+++ poc.module/docroot/modules/custom/jmeter/src/Form/JmeterForm.php
@@ -7,7 +7,6 @@ use Drupal\Core\File\FileSystemInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Psr\Container\ContainerInterface;
-
 class JmeterForm extends FormBase {
 
   /**
@@ -49,16 +48,37 @@ class JmeterForm extends FormBase {
 
   public function buildForm(array $form, FormStateInterface $form_state)
   {
+    $file_dir = '/app/config/jmeter/';
+    $user_dir = glob('/app/config/jmeter/user/*');
+    foreach ($user_dir as $user_file) {
+      $user_file = preg_replace("/(.+)(\.[^.]+$)/", "$1", $user_file);
+      $user_file = explode('/', $user_file);
+      $user_file = end($user_file);
+      $user_file =  str_replace('_' , ' ', $user_file);
+      if (preg_match('/^(?<server>[^\s]+) (?<mode>[^\s]+)$/', $user_file, $matches)) {
+        $user_info[$matches['server']] = $matches['mode'];
+      }
+    }
     foreach(self::JMETER_SERVER as $server) {
+      if (!empty($user_info) && array_key_exists($server, $user_info)) {
+        $user_name = $user_info[$server];
+      } else {
+        $user_name = '';
+      }
+      $form[$server . 'status'] = [
+        '#type' => 'textfield',
+        '#value' => file_exists($file_dir . $server . '.txt') ? $user_name . 'が' . $server . '起動中。' : $server . '停止中。',
+        '#disabled' => true,
+      ];
       $form[$server . 'start'] = [
         '#type' => 'submit',
         '#value' => $server . ' Start',
-        '#disabled' => file_exists('/app/config/jmeter/' . $server . '.txt'),
+        '#disabled' => file_exists($file_dir . $server . '.txt'),
       ];
       $form[$server . 'stop'] = [
         '#type' => 'submit',
         '#value' => $server . ' Stop',
-        '#disabled' => !file_exists('/app/config/jmeter/' . $server . '.txt'),
+        '#disabled' => !file_exists($file_dir . $server . '.txt'),
       ];
     }
     return $form;
@@ -66,6 +86,7 @@ class JmeterForm extends FormBase {
 
   public function submitForm(array &$form, FormStateInterface $form_state)
   {
+    $user_name = \Drupal::currentUser()->getDisplayName();
     $file_name = '';
     $form_input = $form_state->getUserInput();
     $form_input_op = $form_input['op'];
@@ -73,15 +94,32 @@ class JmeterForm extends FormBase {
     // 押したボタン名が、配列のなかにその配列名があるかのエラー処理。
     if (!in_array($jmeter_pressed['0'], self::JMETER_SERVER)) return;
     $file_name = '/app/config/jmeter/' . $jmeter_pressed['0'] . '.txt';
-    file_exists($file_name) ? \Drupal::service('file_system')->unlink($file_name) : touch($file_name);
-    $this->messenger()->addStatus(
-      file_exists($file_name) ? $this->t($jmeter_pressed['0'] . 'が起動しました。') : $this->t($jmeter_pressed['0'] . 'が停止しました。')
-    );
-    // $subjectは入力する文字だから、どのボタンを押したかが入っている。例：「Jmater1 Start」
-    // //preb_matchは、入力されたボタンの文字を区切るためのものっていう認識。
-    // if (preg_match('/[\s]/', $form_input_op, $matches)) {
-    //   kint($matches);
-    //   exit;
-    // }
+    $user_file_name = '/app/config/jmeter/user/' . $jmeter_pressed['0'] . '_' . $user_name . '.txt';
+    if (file_exists($file_name)) {
+      \Drupal::service('file_system')->unlink($file_name);
+      \Drupal::service('file_system')->unlink($user_file_name);
+      $this->messenger()->addStatus($this->t($jmeter_pressed['0'] . 'が停止しました。'));
+    } else {
+      touch($file_name);
+      touch($user_file_name);
+      $this->messenger()->addStatus($this->t($jmeter_pressed['0'] . 'が起動しました。 - 使用者：' . $user_name));
+    }
+    // file_exists($file_name) ? \Drupal::service('file_system')->unlink($file_name) : touch($file_name);
+    // $this->messenger()->addStatus(
+    //   file_exists($file_name) ? $this->t($jmeter_pressed['0'] . 'が起動しました。') : $this->t($jmeter_pressed['0'] . 'が停止しました。')
+    // );
+    // $submit_state[$jmeter_pressed['0'] . 'status'] = [
+    //   '#title_display' => $user_name,
+    // ];
   }
 }
+
+// kint();
+// $form[$jmeter_pressed['0'] . 'state']['user'] = \Drupal::service('file_system');
+// exit;
+// $subjectは入力する文字だから、どのボタンを押したかが入っている。例：「Jmater1 Start」
+// //preb_matchは、入力されたボタンの文字を区切るためのものっていう認識。
+// if (preg_match('/[\s]/', $form_input_op, $matches)) {
+//   kint($matches);
+//   exit;
+// }
