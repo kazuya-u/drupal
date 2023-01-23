5564fbbc9a1a678cc2b3b9a1014121f854db42c4
mochiya-umeki <umeki@mochiya.co>
2023-01-24 01:34:48 +0900

    Directory designation and code organization.

--- poc.module/docroot/modules/custom/jmeter/src/Form/JmeterForm.php
+++ poc.module/docroot/modules/custom/jmeter/src/Form/JmeterForm.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\jmeter\Form;
 
-use DateTime;
 use Drupal;
 use Drupal\Core\File\FileSystemInterface;
 use Drupal\Core\Form\FormBase;
@@ -49,96 +48,55 @@ class JmeterForm extends FormBase {
 
   public function buildForm(array $form, FormStateInterface $form_state)
   {
-    $currentDateTime = new DateTime();
-    $currentDateTime = strtotime($currentDateTime->format('H:i:s'));
-    $file_dir = '/app/config/jmeter/';
-    $user_dir = glob('/app/config/jmeter/sub/*');
-    foreach ($user_dir as $user_file) {
-      $user_file = preg_replace("/(.+)(\.[^.]+$)/", "$1", $user_file);
-      $user_file = explode('/', $user_file);
-      $user_file = end($user_file);
-      $user_file =  str_replace('_' , ' ', $user_file);
-      if (preg_match('/^(?<server>[^\s]+) (?<user>[^\s]+) (?<submitTime>[^\s]+)$/', $user_file, $matches)) {
-        $user_info[$matches['server']] = $matches['user'];
-        $user_info[$matches['server'] . $matches['server']] = $matches['submitTime'];
-      }
-    }
-    foreach(self::JMETER_SERVER as $server) {
-      if (!empty($user_info) && array_key_exists($server, $user_info)) {
-        $user_name = $user_info[$server];
-        $submitDateTime = strtotime($user_info[$server . $server]);
-        $diff = $currentDateTime - $submitDateTime;
-      } else {
-        $user_name = '';
-        $diff = '301';
-      }
-      // kint($diff);
+    $file_dir = explode('docroot', __DIR__);
+    kint($file_dir);
+    $takayama_file = $file_dir['0'] . 'files-private/takayama/start.txt';
+    $file_dir = $file_dir['0'] . 'files-private/jmeter/';
 
-      // !$diff > 300ならボタン無効。
+    foreach(self::JMETER_SERVER as $server) {
+      $user_name = file_exists($file_dir . $server . '.txt') ? file_get_contents($file_dir . $server . '.txt') : '';
       $form[$server . 'status'] = [
         '#type' => 'textfield',
         '#value' => file_exists($file_dir . $server . '.txt') ? $user_name . 'が' . $server . '起動中。' : $server . '停止中。',
         '#disabled' => true,
       ];
-      if ($diff > 10) {
-        $form[$server . 'start'] = [
-          '#type' => 'submit',
-          '#value' => $server . ' Start',
-          // '#disabled' => $diff > 300 && file_exists($file_dir . $server . '.txt') ? false : true,
-          '#disabled' => file_exists($file_dir . $server . '.txt'),
-        ];
-        $form[$server . 'stop'] = [
-          '#type' => 'submit',
-          '#value' => $server . ' Stop',
-          '#disabled' => !file_exists($file_dir . $server . '.txt'),
-        ];
-      } else {
-        $form[$server . 'start'] = [
-          '#type' => 'submit',
-          '#value' => $server . ' Start',
-          // '#disabled' => $diff > 300 && file_exists($file_dir . $server . '.txt') ? false : true,
-          '#disabled' => true,
-        ];
-        $form[$server . 'stop'] = [
-          '#type' => 'submit',
-          '#value' => $server . ' Stop',
-          '#disabled' => true,
-        ];
-      }
-
+      $form[$server . 'start'] = [
+        '#type' => 'submit',
+        '#value' => $server . ' Start',
+        '#disabled' => file_exists($takayama_file) ? true :  file_exists($file_dir . $server . '.txt'),
+      ];
+      $form[$server . 'stop'] = [
+        '#type' => 'submit',
+        '#value' => $server . ' Stop',
+        '#disabled' => !file_exists($file_dir . $server . '.txt'),
+      ];
     }
     return $form;
-  }
-  public function validateForm(array &$form, FormStateInterface $form_state)
-  {
-    parent::validateForm($form, $form_state);
   }
 
   public function submitForm(array &$form, FormStateInterface $form_state)
   {
+
+    $file_dir = explode('docroot', __DIR__);
+    $file_dir = $file_dir['0'] . 'files-private/jmeter/';
+    // 送信時のユーザー名
     $user_name = \Drupal::currentUser()->getDisplayName();
-    $submit_date_time = new DateTime();
-    $file_name = '';
     $form_input = $form_state->getUserInput();
+    // Name of button pressed.
     $form_input_op = $form_input['op'];
-    $jmeter_pressed = preg_split('/\s/', $form_input_op);
-    // 押したボタン名が、配列のなかにその配列名があるかのエラー処理。
-    if (!in_array($jmeter_pressed['0'], self::JMETER_SERVER)) return;
-    $file_name = '/app/config/jmeter/' . $jmeter_pressed['0'] . '.txt';
-    $user_file_name = '/app/config/jmeter/sub/' . $jmeter_pressed['0'] . '_' . $user_name . '_' . $submit_date_time->format('H:i:s') . '.txt';
-    if (file_exists($file_name)) {
-      \Drupal::service('file_system')->unlink($file_name);
-      $user_dir = glob('/app/config/jmeter/sub/*');
-      foreach ($user_dir as $user_file) {
-        if (!strpos($user_file, $jmeter_pressed['0'])) continue;
-        $user_file_name = $user_file;
-        \Drupal::service('file_system')->unlink($user_file_name);
-        $this->messenger()->addStatus($this->t($jmeter_pressed['0'] . 'が停止しました。'));
+    if (preg_match('/^(?<server>[^\s]+) (?<mode>[^\s]+)$/', $form_input_op, $matches) && in_array($matches['server'], self::JMETER_SERVER, TRUE)) {
+      $file_name = $file_dir . $matches['server'] . '.txt';
+      if ($matches['mode'] === 'Start') {
+        // Create file.
+        touch($file_name);
+        $this->messenger()->addStatus($this->t($matches['server'] . 'が起動しました。 - 使用者：' . $user_name));
+        file_put_contents($file_name, $user_name);
       }
-    } else {
-      touch($file_name);
-      touch($user_file_name);
-      $this->messenger()->addStatus($this->t($jmeter_pressed['0'] . 'が起動しました。 - 使用者：' . $user_name));
+      else {
+        // Delete file.
+        \Drupal::service('file_system')->unlink($file_name);
+        $this->messenger()->addStatus($this->t($matches['server'] . 'が停止しました。'));
+      }
     }
   }
 }
